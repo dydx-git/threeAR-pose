8f8dfaf842a9d11cb8a2ea7e63d2988798e038a4
Muhammad Taha <muhammadtahatron@gmail.com>
2021-03-17 01:48:07 +0500

    added nontested poseNet using ml5

--- src/App.svelte
+++ src/App.svelte
@@ -3,25 +3,29 @@
 	import * as THREE from "three/build/three.module";
 	import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 	import Stats from "stats.js/build/stats.min.js";
+	import { poseNet } from "ml5";
 
 	let canvas, ctx, stream;
 	let stats, scene, renderer;
 	let mesh;
 	let camera;
+	let poseNetModel, poses;
 
-	const gltfLoader = new GLTFLoader();
-	gltfLoader.load(
-		"/assets/models/mask.gltf",
-		function (gltf) {
-			scene.add(gltf.scene);
-			mesh = gltf.scene;
-			mesh.position.y = -2;
-		},
-		undefined,
-		function (error) {
-			console.error(error);
-		}
-	);
+	const loadModels = () => {
+		const gltfLoader = new GLTFLoader();
+		gltfLoader.load(
+			"/assets/models/mask.gltf",
+			function (gltf) {
+				scene.add(gltf.scene);
+				mesh = gltf.scene;
+				mesh.position.y = -2;
+			},
+			undefined,
+			function (error) {
+				console.error(error);
+			}
+		);
+	};
 
 	const init = () => {
 		renderer = new THREE.WebGLRenderer({
@@ -49,6 +53,10 @@
 		camera.position.set(0, 0, 5);
 		scene.add(camera);
 	};
+
+	function modelLoaded() {
+		console.log("Model Loaded!");
+	}
 
 	// animation loop
 	function animate() {
@@ -88,14 +96,65 @@
 		return stream;
 	}
 
+	// A function to draw ellipses over the detected keypoints
+	function drawKeypoints() {
+		// Loop through all the poses detected
+		for (let i = 0; i < poses.length; i++) {
+			// For each pose detected, loop through all the keypoints
+			let pose = poses[i].pose;
+			for (let j = 0; j < pose.keypoints.length; j++) {
+				// A keypoint is an object describing a body part (like rightArm or leftShoulder)
+				let keypoint = pose.keypoints[j];
+				// Only draw an ellipse is the pose probability is bigger than 0.2
+				if (keypoint.score > 0.2) {
+					fill(255, 0, 0);
+					noStroke();
+					ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+				}
+			}
+		}
+	}
+
+	// A function to draw the skeletons
+	function drawSkeleton() {
+		// Loop through all the skeletons detected
+		for (let i = 0; i < poses.length; i++) {
+			let skeleton = poses[i].skeleton;
+			// For every skeleton, loop through all body connections
+			for (let j = 0; j < skeleton.length; j++) {
+				let partA = skeleton[j][0];
+				let partB = skeleton[j][1];
+				stroke(255, 0, 0);
+				line(
+					partA.position.x,
+					partA.position.y,
+					partB.position.x,
+					partB.position.y
+				);
+			}
+		}
+	}
+
 	onMount(async () => {
 		ctx = canvas.getContext("webgl");
 		stream = await getMedia({
 			video: true, // navigator.mediaDevices.getSupportedConstraints()
 		});
 		document.querySelector("video").srcObject = stream;
-		if (!init()) animate();
+		if (!init()) {
+			poseNetModel = poseNet(stream, modelLoaded);
+			poseNetModel.on("pose", function (results) {
+				poses = results;
+			});
+			loadModels();
+			animate();
+		}
 	});
+
+	$: if (poses) {
+		drawKeypoints();
+		drawSkeleton();
+	}
 </script>
 
 <main>
